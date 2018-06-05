bd1137ecc1362dea80d168c89903b6efcbe7764a
Lisa Canini <three_nineteen@yahoo.com>
2018-06-04 20:49:20 -0700

    films sorted by episode

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,17 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.route('/')
+    .get((req, res) => {
+        let query = Film
+            .find()
+            query.sort('episode')
+            .then(films => {
+                res.status(200).json(films)
+            })
+            .catch(err => {
+                res.sendStatus(500);
+            })
+    })
 
 module.exports = router;
--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -5,5 +5,15 @@ const Planet = require('./Planet.js');
 const router = express.Router();
 
 // add endpoints here
+router.route('/')
+    .get((req, res) => {
+        Planet.find()
+            .then(planets => {
+                res.status(200).json(planets)
+            })
+            .catch(err => {
+                res.sendStatus(500)
+            })
+    })
 
 module.exports = router;
