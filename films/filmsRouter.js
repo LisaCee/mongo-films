1936e6007e5274724c3c3b8cd55e42b06e658aa8
Alexander <alexander.botello@g.austincc.edu>
2018-06-05 20:57:16 -0500

    Initial boilerplate setup

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -1,9 +1,17 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 const express = require('express');
-
 const Film = require('./Film.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.route('/')
+  .get()
+  .post()
+
+router.route('/:id')
+  .get()
+  .put()
+  .delete()
 
 module.exports = router;
--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,17 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
 const express = require('express');
-
 const Planet = require('./Planet.js');
 
 const router = express.Router();
 
-// add endpoints here
+router.route('/')
+  .get()
+  .post()
+
+router.route('/:id')
+  .get()
+  .put()
+  .delete()
 
 module.exports = router;
