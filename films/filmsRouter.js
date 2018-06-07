d6fe8b4df6173d468c6731fe7eefcb8f6fa549f1
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-06-07 17:54:36 -0500

    Added catch.

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -11,8 +11,10 @@ router.get('/', (req, res) => {
     Film.find()
       .sort('-episode')
       .select('episode title planets characters')
-      .populate('planets')
+      .populate('planets', 'name climate terrain gravity diameter -_id')
+      .populate('characters', 'name')
       .then( films => res.status(200).json(films))
+      .catch( err => res.sendStatus(500))
 })
 
 module.exports = router;
