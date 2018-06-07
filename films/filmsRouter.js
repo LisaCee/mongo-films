805004951ff1ba0bfc1c090b792a11be12c7f947
rcs784 <rcstinson@hotmail.com>
2018-06-07 00:33:26 -0400

    stopping point for 6/6

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,11 +5,19 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  Film.find()
+  const films = Film.find()
     .sort('episode')
     .select('title producer release_date')
-    .populate('characters', '_id, name, gender, height, skin_color, hair_color, eye_color')
-    .then(response => res.json(response))
+    .populate('characters', '_id name gender height skin_color hair_color eye_color')
+    .populate('planets', 'name climate terrain gravity diameter')
+  if (req.query) {
+    if (req.query.producer) {
+      films.where({ producer: new RegExp(req.query.producer, 'i') });
+    } else if (req.query.released) {
+      films.where({ release_date: new RegExp(req.query.released) });
+    }
+  }
+  films.then(response => res.json(response))
     .catch(err => res.status(500).json(err.message));
 });
 
