e2a7c69ca3d78b4740c75124af5f1de990a908ab
Lisa Canini <three_nineteen@yahoo.com>
2018-06-06 20:27:04 -0700

    get by gender working

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -6,23 +6,21 @@ const router = express.Router();
 
 // add endpoints here
 router.get('/', (req, res) => {
-    Character.find()
-        .then(chars => res.status(200).json(chars))
+    const { gender, minheight } = req.query;
+    const query = Character.find()
+    if (gender !== undefined) {
+        const filter = new RegExp(gender, 'i')
+        query.where({ gender: filter })
+    }
+    if (minheight !== undefined) {
+        query.find().where('height').gt(minheight)
+    }
+    query.then(chars => res.status(200).json(chars))
         .catch(err => {
             res.status(500).json(err)
         })
 })
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-    Character.findById(id)
-        .populate('homeworld', 'name climate')
-        .select('name height birth_year')
-        .then(char => res.status(200).json(char))
-        .catch(err => {
-            res.status(500).json(err)
-        })
-})
 // router.route('/')
 //     .get((req, res) => {
 //         const { gender, minheight } = req.query
@@ -39,21 +37,21 @@ router.get('/:id', (req, res) => {
 //             })
 //     })
 
-// router.route('/:id')
-//     .get((req, res) => {
-//         const { id } = req.params;
-//         Character
-//             .findById(id)
-//             .then(character => {
-//                 if (character !== null) {
-//                     res.status(200).json(character);
-//                 } else {
-//                     res.sendStatus(404);
-//                 }
-//             })
-//             .catch(err => {
-//                 res.status(500).json(err);
-//             })
-//     })
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Character
+        .findById(id)
+        .populate('homeworld', 'name')
+        .then(character => {
+            if (character !== null) {
+                res.status(200).json(character);
+            } else {
+                res.status(404).json({ error: 'There is no character with that id' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
 
 module.exports = router;
