a339b576d75aabc6d85976ad39f58ceefd7e405e
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-06-06 19:16:49 -0500

    Finished route for filmsRouter

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -7,4 +7,17 @@ const router = express.Router();
 // add endpoints here
 // so here we go
 
+router.get('/', (req, res) => {
+    Film.find()
+      .sort('episode')
+      .select('title producer relase_date')
+      .populate('characters', '_id, name, gender, height, skin_color, hair_color, eye_color')
+      .then(response => {
+          res.json(response);
+      })
+      .catch(err => {
+          res.status(500).json(err.message);
+      })
+})
+
 module.exports = router;
