e0457e8978cf61ea4603d76db9c98c8585459ad6
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-06-07 18:42:51 -0500

    Going to try to use JavaScript regex instead on released filter.

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -8,12 +8,22 @@ const router = express.Router();
 // so here we go
 
 router.get('/', (req, res) => {
-    Film.find()
+    const { producer, released } = req.query;
+    let query = Film.find()
       .sort('-episode')
-      .select('episode title planets characters')
-      .populate('planets', 'name climate terrain gravity diameter -_id')
-      .populate('characters', 'name')
-      .then( films => res.status(200).json(films))
+      .select('episode title producer released_date')
+      // .populate('planets', 'name climate terrain gravity diameter -_id')
+      // .populate('characters', 'name')
+    if (producer) {
+        // javascript regular expression
+        const filter = new RegExp(producer, 'i')
+        query.where({producer: filter})
+    }
+    // native mongo regular expression
+    if (released) {
+        query.where({release_date: { $regex: released, $options: 'i' }})
+    }
+      query.then( films => res.status(200).json(films))
       .catch( err => res.sendStatus(500))
 })
 
