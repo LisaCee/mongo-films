607db6922458fd586ff4226195edacdf41f978e4
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-06-07 13:58:46 -0500

    Following along with Dan.

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,8 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+    Character.find()
+})
 
 module.exports = router;
