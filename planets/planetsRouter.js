8a221cee08cf7fe4a77c0e483a6a204644a143c0
Sean Valdivia <irishseanvaldivia@gmail.com>
2018-06-07 19:25:30 -0500

    Added characters and species to planets.

--- planets/planetsRouter.js
+++ planets/planetsRouter.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
 const Planet = require('./Planet.js');
-
+const Character = require('../characters/Character');
+const Species = require('../species/Specie');
 const router = express.Router();
 
 // add endpoints here
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const char = Character.find({homeworld: id})
+    const species = Species.find({homeworld: id})
+
+    Promise.all([char, species])
+      .then( results => {
+          const [ characters, species ] = results;
+          res.status(200).json({ characters, species })
+      })
+      .catch( err => res.sendStatus(500))
+})
 
 module.exports = router;
