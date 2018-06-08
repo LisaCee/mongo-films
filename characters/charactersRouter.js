2a35992484f44e57d8454db0b7e5bc4b58f60fde
Bhavik Ravani <ravanibhavik@gmail.com>
2018-06-07 22:48:46 -0400

    completed api/characters/:id

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,9 +1,28 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Film = require('../films/Film.js');
 
 const router = express.Router();
 
 // add endpoints here
+
+
+router.get("/", (req, res) => {
+    Character.find()
+    .then(characters => res.status(200).json(characters))
+})
+
+router.get("/:id", (req, res) => {
+    let { id } = req.params;
+    const charByID = Character.findById(id)
+        .populate('homeworld')
+        .select('name gender skin_color hair_color height eye_color birth_year')
+    const movies = Film.where({ characters: id })
+        .select('title producer director episode release_date')
+    Promise.all([charByID, movies])
+        .then(response => res.status(200).json(response))
+        .catch(err => res.status(500).json({ err: err.message }))
+})
 
 module.exports = router;
