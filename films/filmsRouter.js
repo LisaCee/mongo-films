f265fd4a22881a1cbda2f0c140925924f9bab8b5
Bhavik Ravani <ravanibhavik@gmail.com>
2018-06-07 21:52:54 -0400

    completed /api/films and /api/films?producer=gary+kurtz

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -6,4 +6,28 @@ const router = express.Router();
 
 // add endpoints here
 
+router.get("/", (req, res) => {
+    let { producer } = req.query;
+    let query, producer_name;
+    if (producer !== undefined) {
+        producer_name = new RegExp(producer, 'i');
+        query = Film.find({ producer: producer_name })
+    } else {
+        query = Film.find()
+    }
+    query.sort('episode')
+    // Film.find({ producer: { $regex: producer, $options: 'i' } }).sort('episode')
+    .select("episode title producer director release_date planets characters")
+    .populate('planets', 'name climate terrain gravity diameter-_id')
+    .populate('characters', 'name gender height skin_color hair_color eye_color')
+    .then(films => {
+        return res.status(200).json(films);
+    })
+    .catch(error => {
+        return res.status(500).json({
+            errorMessage: "Something went wrong. Please try again later."
+        })
+    })
+})
+
 module.exports = router;
