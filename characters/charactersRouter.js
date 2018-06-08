5a96f679626cc25bc0ffc1bc2e9e2af60d60ad48
Lisa Canini <three_nineteen@yahoo.com>
2018-06-08 10:59:16 -0700

    get films in order, with characters and planets working, producer, year and character by id with homeworld and movies working

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,9 +1,36 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Film = require('../films/Film')
 
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+    Character.find()
+        .then(characters => {
+            res.status(200).json(characters);
+        })
+})
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Character.findById(id)
+        .populate('homeworld', 'name')
+        .then(character => {
+            if (character) {
+                Film.find({ characters: id })
+                    .select('title -_id')
+                    .then(films => {
+                        res.status(200).json([character, films])
+                    })
+            } else {
+                res.status(404).json({error: 'The character with that ID does not exist'})
+            }
+        })
+        .catch(err => {
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,26 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+    let { producer, released } = req.query;
+    let query = Film.find()
+        .sort('episode')
+        .select('title episode -_id release_date producer')
+        // .populate('characters', '_id name gender height skin_color hair_color eye_color')
+        .populate('planets', 'name climate terrain gravity diameter')
+    if (producer) {
+        const producerFilter = new RegExp(producer, 'i')
+        query.where({ producer: producerFilter })
+    }
+    if (released) {
+        query.where({ release_date: { $regex: released } })
+    }
+    query.then(films => {
+        res.status(200).json(films)
+    })
+        .catch(err => {
+            res.sendStatus(500);
+        })
+})
 
 module.exports = router;
